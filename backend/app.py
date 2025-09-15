51a285b7a3cd85303fdf2de1b95ab9eef5db71b2
RaghuNandan <raghu.m2k5@gmail.com>
2025-09-15 18:16:37 +0530

    production updates

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from Model.Model import MyModel
-
+import os
 app = Flask(__name__)
 
 CORS(app, resources={
     r"/*": {
-        "origins": ["http://localhost:3000", "http://127.0.0.1:3000"],
+        "origins": ["http://localhost:3000", "http://127.0.0.1:3000" , "*"],
         "methods": ["GET", "POST", "OPTIONS"],
         "allow_headers": ["Content-Type"]
     }
@@ -88,4 +88,5 @@ def summary():
 '''
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
